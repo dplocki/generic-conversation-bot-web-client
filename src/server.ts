1cd1a1370ef39861d7c89ede8131a98a2cc7e8f9
Dawid Płocki <dawid.plocki@gmail.com>
2021-07-15 20:17:34 +0200

    Use koa-ejs, koa-session and koa-static

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,67 @@
-import app from './app';
-import router from './router';
+import * as Koa from 'koa';
+import * as HttpStatus from 'http-status-codes';
+import * as path from 'path';
+import * as render from 'koa-ejs';
+import * as serve from 'koa-static';
+import * as session from 'koa-session';
+import * as Router from 'koa-router';
+import * as bodyParser from 'koa-bodyparser';
 
-const PORT:number = Number(process.env.PORT) || 3000;
+const PORT: number = Number(process.env.PORT) || 3000;
+const app: Koa = new Koa();
+const router: Router = new Router();
+
+router.get('/', async (context: any, next) => {
+  if (context.path === '/favicon.ico') {
+    return;
+  }
+
+  await context.render('index', {
+    chatHistory: context.session.chatHistory ? context.session.chatHistory : [],
+  });
+});
+
+router.post('/', async (context, next) => {
+  if (context.session.isNew) {
+    context.session.chatHistory = [];
+  }
+
+  context.session.chatHistory = [
+    ...context.session.chatHistory,
+    context.request.body.value,
+  ];
+
+  context.redirect('.');
+});
+
+app.keys = ['secret'];
+app.use(bodyParser());
+app.use(session({}, app));
+
+render(app, {
+  root: path.join(__dirname, '/../public'),
+  layout: 'index',
+  viewExt: 'html',
+  cache: false,
+  debug: false,
+});
+
+app.use(async (context: Koa.Context, next: () => Promise<any>) => {
+  try {
+    await next();
+  } catch (error) {
+    context.status = error.statusCode
+      || error.status
+      || HttpStatus.StatusCodes.INTERNAL_SERVER_ERROR;
+    error.status = context.status;
+    context.body = { error };
+    context.app.emit('error', error, context);
+  }
+});
 
 app
+  .on('error', console.error)
   .use(router.routes())
-  .use(router.allowedMethods());
-
-app.listen(PORT);
+  .use(serve(path.join(__dirname, '/../public')))
+  .use(router.allowedMethods())
+  .listen(PORT);
